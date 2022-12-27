f83e5934f4641e61c51f815cadd9b72df9d65570
skraft-redhat <57144373+skraft-redhat@users.noreply.github.com>
2022-12-27 13:31:26 +0100

    Update GreetingResource.java

--- inner-outer-loop-demo/src/main/java/org/acme/GreetingResource.java
+++ inner-outer-loop-demo/src/main/java/org/acme/GreetingResource.java
@@ -16,6 +16,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from stage " + stage + ". This was developed with OpenShift DevSpaces";
+        return "Hello from stage " + stage + ". This was developed with OpenShift DevSpaces. v2";
     }
 }
