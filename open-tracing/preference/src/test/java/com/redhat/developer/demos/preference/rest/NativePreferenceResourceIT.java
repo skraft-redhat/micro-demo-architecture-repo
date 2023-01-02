55e43d2af3dd532cf675fba8def6401fc9e74ce8
Stephan Kraft <skraft@redhat.com>
2023-01-02 10:48:15 +0100

    Adding Service Mesh demo

--- open-tracing/preference/src/test/java/com/redhat/developer/demos/preference/rest/NativePreferenceResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.developer.demos.preference.rest;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativePreferenceResourceIT extends PreferenceResourceTest {
-
-    // Execute the same tests but in native mode.
-}
