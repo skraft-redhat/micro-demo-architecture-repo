fe38915ba068be5137e9805a561402278b04c730
Stephan Kraft <skraft@redhat.com>
2023-01-23 11:15:47 +0100

    Adding Client Credentials Demo

---
+++ RBAC-JWT-Demo/quarkus-keycloak-oidc-client-demo/client/src/main/java/org/acme/security/openid/connect/client/FrontendResource.java
@@ -0,0 +1,35 @@
+package org.acme.security.openid.connect.client;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import io.smallrye.mutiny.Uni;
+
+@Path("/frontend")
+public class FrontendResource {
+    @Inject
+    @RestClient
+    RestClientWithOidcClientFilter restClientWithOidcClientFilter;
+
+    @Inject
+    @RestClient
+    
+    @GET
+    @Path("user-name-with-oidc-client-token")
+    @Produces("text/plain")
+    public Uni<String> getUserNameWithOidcClientToken() {
+        return restClientWithOidcClientFilter.getUserName();
+    }
+    
+    @GET
+    @Path("admin-name-with-oidc-client-token")
+    @Produces("text/plain")
+    public Uni<String> getAdminNameWithOidcClientToken() {
+	    return restClientWithOidcClientFilter.getAdminName();
+    }
+    
+}
