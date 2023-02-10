d1764166eee8e3c4ddaff3ddc77ce1305105812c
Stephan Kraft <skraft@redhat.com>
2023-02-10 16:40:10 +0100

    Updating RBAC demo

---
+++ RBAC-JWT-Demo/3-jwt/client/src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,52 @@
+package org.acme;
+
+import javax.annotation.security.PermitAll;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.logging.Logger;
+
+@Path("/client")
+public class GreetingResource {
+
+    private static final Logger LOG = Logger.getLogger(GreetingResource.class);
+
+
+    @Inject
+    JsonWebToken jwt;
+
+    @RestClient
+    @Inject
+    ServerInterface serverInterface;
+
+    @GET
+    @Path("publicService")
+    @PermitAll
+    @Produces(MediaType.TEXT_PLAIN)
+    public String calling_publicService() {
+        LOG.info("Received request - forwarding to server");
+        return serverInterface.publicService();
+    }
+
+    @GET
+    @Path("userService")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String calling_UserService() {
+        LOG.info("Received request - forwarding to server");
+        return serverInterface.userService();
+    }
+
+    @GET
+    @Path("adminService")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String calling_AdminService() {
+        LOG.info("Received request - forwarding to server");
+
+        return serverInterface.adminService();
+    }
+}
